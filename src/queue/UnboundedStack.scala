4f58ee9aa83a756a0c20e6c0272f03c737e5cb75
Dan Fox <iamdanfox@gmail.com>
2015-01-19 19:52:00 +0000

    Tidy up

--- src/queue/UnboundedStack.scala
+++ src/queue/UnboundedStack.scala
@@ -1,87 +1,85 @@
 package queue
 import java.util.concurrent.atomic.{ AtomicReferenceArray, AtomicReference }
+import UnboundedStack.{Index, Chunk, ChunkUpdate, CellUpdate, V}
 
 class UnboundedStack[T: scala.reflect.ClassTag] {
 
-  trait V {}
-  case class Null() extends V
-  case class Datum(x: T) extends V
+  val state: AtomicReference[(Index, ChunkUpdate, CellUpdate)] =
+    new AtomicReference(((0, 0), None, ((0, 0), null, Null())))
 
-  type Chunk = AtomicReferenceArray[V]
-  type Index = (Int, Int)
-
-  val arrays = new AtomicReferenceArray[Chunk](10)
+  val arrays = new AtomicReferenceArray[Chunk](32)
   arrays.set(0, new Chunk(1))
 
-  val cellUpdate = ((0, 0), null.asInstanceOf[V], Null().asInstanceOf[V])
-  val chCAS = None.asInstanceOf[Option[(Int, Chunk, Chunk)]]
-  val nextPush = (0, 0)
-  val state = new AtomicReference((nextPush, chCAS, cellUpdate))
-
-  /**
-   * Try to apply cellUpdate, (expanding/contracting as necessary) then
-   * create new state, try to put back
-   */
   @annotation.tailrec final def push(x: T): Boolean = {
-    val oldState @ (nextPushCell, chCAS, (index, expect, replace)) = state.get()
-    if (chunkCAS(chCAS) && cellCAS(index, expect, replace)) {
-      val oldVal = arrayGet(nextPushCell)
-      val newState = (increment(nextPushCell), fitTo(nextPushCell), (nextPushCell, oldVal, Datum(x)))
-      if (this.state.compareAndSet(oldState, newState))
-        return true
+    val oldState @ (currentPushCell, chunkUpdate, cellUpdate) = state.get()
+    increment(currentPushCell) match {
+      case None => return false
+      case Some(incremented) =>
+        if (chunkCAS(chunkUpdate) && cellCAS(cellUpdate)) {
+          val oldVal = arrayGet(currentPushCell)
+          val newState = (incremented, fitChunksTo(currentPushCell), (currentPushCell, oldVal, Datum(x)))
+          if (this.state.compareAndSet(oldState, newState))
+            return true
+        }
     }
     push(x) // otherwise recurse
   }
 
   @annotation.tailrec final def pop: Option[T] = {
-    val oldState @ (nextPushCell, chCAS, (index, expect, replace)) = state.get()
-    decrement(nextPushCell) match {
+    val oldState @ (pushCell, chunkUpdate, cellUpdate) = state.get()
+    decrement(pushCell) match {
       case None => return None
-      case Some(decremented) => {
-        if (chunkCAS(chCAS) && cellCAS(index, expect, replace)) {
-          val rtn = arrayGet(decremented)
-          val newState = (decremented, fitTo(decremented), (decremented, rtn, Null()))
-          if (this.state.compareAndSet(oldState, newState)) 
+      case Some(currentPopCell) =>
+        if (chunkCAS(chunkUpdate) && cellCAS(cellUpdate)) {
+          val rtn = arrayGet(currentPopCell)
+          val newState = (currentPopCell, fitChunksTo(currentPopCell), (currentPopCell, rtn, Null()))
+          if (this.state.compareAndSet(oldState, newState))
             return Some(rtn.asInstanceOf[Datum].x)
         }
-      }
     }
     pop // otherwise recurse
   }
 
-  def cellCAS(index: Index, expect: V, replace: V): Boolean = {
+  def cellCAS(cellUpdate: CellUpdate): Boolean = {
+    val (index, expect, replace) = cellUpdate
     val arr = arrays.get(index._1)
     return arr != null &&
       (arr.get(index._2) == replace ||
         arr.compareAndSet(index._2, expect, replace))
   }
 
-  def chunkCAS(chCAS: Option[(Int, Chunk, Chunk)]): Boolean =
-    chCAS match {
+  def chunkCAS(chunkUpdate: ChunkUpdate): Boolean =
+    chunkUpdate match {
       case None => true
       case Some((chIndex, expect, replace)) =>
-        arrays.get(chIndex) == replace || arrays.compareAndSet(chIndex, expect, replace)
+        return arrays.get(chIndex) == replace ||
+          arrays.compareAndSet(chIndex, expect, replace)
     }
 
-  def fitTo(index: Index): Option[(Int, Chunk, Chunk)] = {
+  def fitChunksTo(index: Index): ChunkUpdate = {
     if (arrays.get(index._1) == null) { // ie we're submitting a push to a chunk that doesn't exist yet
-      val newChunk = new Chunk(scala.math.pow(2, index._1).asInstanceOf[Int])
+      val newChunk = new Chunk((scala.math.pow(2, index._1)).asInstanceOf[Int])
       Some((index._1, null, newChunk))
-    }else {
-      val afterChunk = arrays.get(index._1 + 1)
-      if (afterChunk != null) // no need to keep a whole empty one
-        Some(index._1 + 1, afterChunk, null)
+    } else { // current push exists... can we delete a later one?
+      val afterCurrent = arrays.get(index._1 + 1)
+      if (afterCurrent != null)
+        Some(index._1 + 1, afterCurrent, null)
       else
         None
     }
   }
 
-  // returns the next Index we can write to after filling argument index
-  def increment(index: Index): Index =
-    if (index._2 == scala.math.pow(2, index._1) - 1)
-      (index._1 + 1, 0)
+  /**
+   *  returns the next Index we can write to after filling argument index
+   */ 
+  def increment(index: Index): Option[Index] =
+    if (index._1 < 32)
+      if (index._2 == scala.math.pow(2, index._1) - 1)
+        Some((index._1 + 1, 0))
+      else
+        Some((index._1, index._2 + 1))
     else
-      (index._1, index._2 + 1)
+      None // will overflow INT_MAX on the second param
 
   def decrement(index: Index): Option[Index] =
     if (index._2 > 0)
@@ -89,7 +87,7 @@ class UnboundedStack[T: scala.reflect.ClassTag] {
     else if (index._1 > 0)
       Some((index._1 - 1, arrays.get(index._1 - 1).length - 1))
     else
-      None
+      None // can't decrement below (0,0)
 
   def arrayGet(pair: Index): V = {
     val arr = arrays.get(pair._1)
@@ -98,19 +96,15 @@ class UnboundedStack[T: scala.reflect.ClassTag] {
     else
       arr.get(pair._2)
   }
+
+  case class Null() extends V
+  case class Datum(x: T) extends V
 }
 
 object UnboundedStack {
-
-  def main(args: Array[String]) = {
-    val st = new UnboundedStack[String]()
-    println(st.push("a"))
-    println(st.push("b"))
-    println(st.push("c"))
-    println(st.pop)
-    println(st.pop)
-    println(st.pop)
-    println(st.pop)
-    println("Done.")
-  }
+  trait V {}
+  type Chunk = AtomicReferenceArray[V]
+  type Index = (Int, Int)
+  type CellUpdate = (Index, V, V)
+  type ChunkUpdate = Option[(Int, Chunk, Chunk)]
 }
